1466cfb41832dd686fa3b56e68be59f50e065df1
Pema@777 <pemanamgurtamang7@gmail.com>
2023-03-02 18:42:12 +0545

    multiple catch

---
+++ multiplecatch.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+    void test(int x)
+    {
+        try
+        {
+          if (x==0) throw x;
+          if(x==1) throw 1.0;
+        }
+        catch(int m)
+        {
+            cout<<"caught an integer"<<endl;
+        }
+        catch(double d)
+        {
+            cout<<"caught a double";
+        }
+    }
+int main()
+{
+    test(0);
+    test(1);
+    test(2);
+    return 0;
+}
